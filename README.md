3dbc8009e28a1fe9f2a9b3f1c5e42002a56bd2c9
Ashraf Hasan Alsheikh <alsheikh813@mail.com>
2018-10-04 14:11:02 +0300

    Version 3

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# HI every body
+# This project Created By 
+# Rashad Majdoba
+# Ashraf Hasan
+# RBK COHORT 5
+# oct 4,2018
+# Description
+
+# this is a demo about  small shop website that contan the following 
+# 1.the main page with promotion slider
+# 2.the category button that will show us deferent list of category we use only 3 category
+# 2.1 when you click on category it will show you more detales
+# 3. product   button that will show us deferent list of product we use only 6 product
+# 3.1 when you click on product it will show you more detales
+# 4 we have a cart to count how many Quantty you want and he will count the total price of these product 
+# 5 the rest project still under work 
+
+# we selection this type of project to improve our applty to work in multy function project 
+# we try to use bootstrap and css 
+# we use JQ to munipulate our project and connect the pages with each ather 
+# we use array to save our data insted of database 
+# we use the factory function to create object and array to use inside the html 
+# in JQ we use this in multy places to deside witch element is clicked at and we use attr to pass value from 1 function to athor
--- main.js
+++ main.js
@@ -7,44 +7,46 @@ $( document ).ready(function() {
 
 ////////////////////////////////////////////////
 
-    var prodL = []
-    var cusL = []
-    var catL = []
-    var cart =[]
+var prodL = []
+var cusL = []
+var catL = []
+var cart =[]
 
-    catL[0] = catF('food',"img/cat/wat.jpg");
-    catL[1] = catF('drink',"img/cat/food.jpg");
+catL[0] = catF('Food',"img/cat/food.jpeg","Dairy,Fruits,... ","Dairy,Fruits,Grains, beans and legumes,Meat,Vegetables, ");
+catL[1] = catF('Drink',"img/cat/wat.jpeg","tea,pepsi,...","tea ,green tea,cola,orange ,mango ");
+catL[2] = catF('Candy',"img/cat/candy.jpeg","Circus peanuts,Candy corn,...","Circus peanuts,Candy corn, Tootsie Rolls , Smarties , Licorice.");
 
-    prodL[0] = prodF("Doritos", 0.9 , "Doritos 100 gm", "hellolemon 500 gmlemon 500 gmlemon 500 gmlemon 500 gm", "img/homepage/pro1.jpeg",0);
 
-    prodL[1] = prodF("Mr.Chips", 0.90 , "Mr.Chips 100 gm","hello lemon 500 gmlemon 500 gmlemon 500 gmlemon 500 gm", "img/homepage/pro2.jpg",1);
+prodL[0] = prodF("Doritos", 0.9 , "Doritos 100 gm", "hellolemon 500 gmlemon 500 gmlemon 500 gmlemon 500 gm", "img/homepage/pro1.jpeg",0);
 
-    prodL[2] = prodF("lemon", 4.30 , "lemon 500 gm", "hello lemon 500 gmlemon 500 gmlemon 500 gmlemon 500 gmlemon 500 gm","img/homepage/pro3.jpg",0);
+prodL[1] = prodF("Mr.Chips", 0.90 , "Mr.Chips 100 gm","hello lemon 500 gmlemon 500 gmlemon 500 gmlemon 500 gm", "img/homepage/pro2.jpg",1);
 
-    prodL[3] = prodF("Banana", 2.00 , "Banana 500 gm", "hello","img/homepage/pro4.jpg",1);
+prodL[2] = prodF("lemon", 4.30 , "lemon 500 gm", "hello lemon 500 gmlemon 500 gmlemon 500 gmlemon 500 gmlemon 500 gm","img/homepage/pro3.jpg",0);
 
-    prodL[4] = prodF("Apple", 3.5 , "Apple 500 gm", "hello","img/homepage/pro5.jpg",0);
+prodL[3] = prodF("Banana", 2.00 , "Banana 500 gm", "hello","img/homepage/pro4.jpg",1);
 
-    prodL[5] = prodF("Struppary", 5.00 , "Strawbery 500 gm", "hello","img/homepage/strawberries.png",1);
+prodL[4] = prodF("Apple", 3.5 , "Apple 500 gm", "hello","img/homepage/pro5.jpg",0);
 
-    cusL[0] = cusF("Name1", "123")
+prodL[5] = prodF("Struppary", 5.00 , "Strawbery 500 gm", "hello","img/homepage/strawberries.png",1);
 
-    cusL[1] = cusF("Name2", "321")
+cusL[0] = cusF("Name1", "123")
 
-    cusL[2] = cusF("Name3", "213")
+cusL[1] = cusF("Name2", "321")
+
+cusL[2] = cusF("Name3", "213")
 
 	// console.log(prodL)
 	// console.log(cusL)
 	// console.log(catL)
 
 	var $mainContVal = $('<div></div>')
-    var $mainContID = $('#mainContID')
+	var $mainContID = $('#mainContID')
 
     //console.log($mainContVal)
 
     var $drob ='';
     $drob = '<div id="Quanty" class="form-group">'+'<label for="sel1">Prodects:</label>'
-	+'<select>'
+    +'<select>'
     for(var i=0;i<=10;i++){
     	$drob+="<option >"+i+"</option>"
     }
@@ -54,29 +56,31 @@ $( document ).ready(function() {
 // Factory Functions:
 ////////////////////////////////////////////////
 
-    function prodF(Name, Price, DescriptionApstrac, DescriptionAll, Img, CatID){
-    	return {
-    		Name: Name,
-    		Price: Price,
-    		DescriptionApstrac: DescriptionApstrac,
-    		DescriptionAll: DescriptionAll,
-    		Img: Img,
-    		catN: catL[CatID]
-    	}
-    }
+function prodF(Name, Price, DescriptionApstrac, DescriptionAll, Img, CatID){
+	return {
+		Name: Name,
+		Price: Price,
+		DescriptionApstrac: DescriptionApstrac,
+		DescriptionAll: DescriptionAll,
+		Img: Img,
+		catN: catL[CatID]
+	}
+}
 
-    function cusF(Name, Pass) {
-    	return {
-    		Name: Name,
-    		Pass: Pass,
-    	}
-    }
+function cusF(Name, Pass) {
+	return {
+		Name: Name,
+		Pass: Pass,
+	}
+}
 
-    function catF(Name, Img) {
+function catF(Name, Img,DescriptionApstrac, DescriptionAll) {
     	// catF Start
     	return {
     		Name: Name,
     		Img: Img,
+    		DescriptionApstrac : DescriptionApstrac,
+    		DescriptionAll : DescriptionAll
     	}
     	// catF Ends
     }
@@ -92,23 +96,27 @@ function createCatL(){
 		str +=			'<h2><a href="#"><span>' + catL[i].Name +'</span></a></h2>'
 		str +=			'<div class="card" >'
 		str +=				'<img class="card-img-top" src="'+ catL[i].Img +' " alt="Card image cap" >'
+		str += '<div class="">' + catL[i].DescriptionApstrac + '</div>';
+
 		str +=			'</div>'
 		str +=		'</div>'
 	}
-		str+=	'</div>'
+	str+=	'</div>'
 	return str;
 }
 
 function createCat(num){
 	var str =	'<div class="container row">'
 
-		str += 	 	'<div class="container col-md-4">'
-		str +=			'<h2><a href="#"><span>' + catL[num].Name +'</span></a></h2>'
-		str +=			'<div class="card" >'
-		str +=				'<img class="card-img-top" src="'+ catL[num].Img +' " alt="Card image cap" >'
-		str +=			'</div>'
-		str +=		'</div>'
-		str+=	'</div>'
+	str += 	 	'<div class="container col-md-4">'
+	str +=			'<h2><a href="#"><span>' + catL[num].Name +'</span></a></h2>'
+	str +=			'<div class="card" >'
+	str +=				'<img class="card-img-top" src="'+ catL[num].Img +' " alt="Card image cap" >'
+	str += '<div class="">' + catL[num].DescriptionAll + '</div>';
+
+	str +=			'</div>'
+	str +=		'</div>'
+	str+=	'</div>'
 	return str;
 }
 
@@ -177,8 +185,8 @@ function cartL(){
 		str += '</div>'
 		
 	}
-	str += '<div class= " ">' + sum  + 'JOD'+ '</div>';
 	str+='</div>'
+	str +='<div id="sum"><h2>THE TOTAL IS : ' + '<span id="sumv">' + sum + '</span>'  + ' JOD</h2>'+ '</div>';
 	//str+='</div>'
 
 
@@ -199,13 +207,13 @@ function searchP(name) {
 }
 
 function searchC(name) {
-	alert("searhC " + name)
+	//alert("searhC " + name)
 	for(var i = 0; i < catL.length; i++) {
 		if (catL[i].Name == name) {
 			//
-			alert(catL[i].Name, i)
+			//alert(catL[i].Name, i)
 			//
-			alert( i)
+			//alert( i)
 			return i
 		}
 	}
@@ -220,37 +228,37 @@ function searchC(name) {
 $('#catID').click(function(){
 // alert("Category List")
 
-	$mainContVal.html(createCatL());
-	$mainContID.html('')
-	$mainContVal.appendTo($mainContID)
-	$('#slideID').hide()
+$mainContVal.html(createCatL());
+$mainContID.html('')
+$mainContVal.appendTo($mainContID)
+$('#slideID').hide()
 	//$('#mainContID')
-			$('span').on('click', function(){
+	$('span').on('click', function(){
 		// alert("Span")
-			var catName = $(this).text()
-			alert(catName)
+		var catName = $(this).text()
+		//alert(catName)
 
-			$mainContID.html('')
-			$mainContVal.html(createCat(searchC(catName)));
-			$mainContVal.appendTo($mainContID)
-		});
+		$mainContID.html('')
+		$mainContVal.html(createCat(searchC(catName)));
+		$mainContVal.appendTo($mainContID)
+	});
 });
 
 
 //////
 $('#prodsID').click(function(){
 //	alert("Products List")
-	$mainContVal.html(createProdL());
-	$mainContID.html('')
-	$mainContVal.appendTo($mainContID)
-	$('#slideID').hide()
+$mainContVal.html(createProdL());
+$mainContID.html('')
+$mainContVal.appendTo($mainContID)
+$('#slideID').hide()
 
-		$('span').on('click', function(){
+$('span').on('click', function(){
 		// alert("Span")
-			var prodName = $(this).text()
-			alert(prodName)
+		var prodName = $(this).text()
+		//alert(prodName)
 
-			$mainContID.html('')
+		$mainContID.html('')
 			$mainContVal.html(createProd(searchP(prodName))); //searchP(prodName)
 			$mainContVal.appendTo($mainContID)
 
@@ -258,14 +266,14 @@ $('#prodsID').click(function(){
 				var id= $(this).attr('pid');
 				var val = $('#Quanty :selected').text()
 				cart.push([id,val])
-				console.log(cart);
+				//console.log(cart);
 			});
 		});
 });
 
 
 $('#cartId').click(function(){
-	alert("CART List")
+	//alert("CART List")
 	$mainContVal.html(cartL());
 	$mainContID.html('')
 	$mainContVal.appendTo($mainContID)
@@ -287,12 +295,32 @@ $('#cartId').click(function(){
 				console.log(cart);
 			});
 		});*/
+	});
+
+$('#signUpID').click(function(){
+// alert("Category List")
+	$SiteUnderConstractin = "<center><h1>COMUNG SOON SS</h1><center>"
+	$mainContVal.html($SiteUnderConstractin);
+	$mainContID.html('')
+	$mainContVal.appendTo($mainContID)
+	$('#slideID').hide()
 });
+
+$('#loginID').click(function(){
+// alert("Category List")
+	$SiteUnderConstractin = "<center><h1>COMUNG SOON LL</h1><center>"
+	$mainContVal.html($SiteUnderConstractin);
+	$mainContID.html('')
+	$mainContVal.appendTo($mainContID)
+	$('#slideID').hide()
+});
+
+
 
 //////////
 $('#prodID').click(function(){
 //	alert("One Product")
-	$('#mainContID').html(createProd(0));
+$('#mainContID').html(createProd(0));
 });
 
 });
